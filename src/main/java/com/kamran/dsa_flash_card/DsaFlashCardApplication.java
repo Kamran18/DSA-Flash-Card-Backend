89c1957723aa960600a6f3c9c06565e8b6ae32c1
KAMRAN ASHRAF <umar17ash@gmail.com>
2022-05-29 15:28:18 +0530

    initial commit

---
+++ src/main/java/com/kamran/dsa_flash_card/DsaFlashCardApplication.java
@@ -0,0 +1,13 @@
+package com.kamran.dsa_flash_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaFlashCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaFlashCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamran/dsa_flash_card/DsaFlashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamran.dsa_flash_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaFlashCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
